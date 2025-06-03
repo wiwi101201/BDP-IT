074913cfe8c13b16c299b3bc8bd5f071a021fc0b
William <114482447+wiwi101201@users.noreply.github.com>
2025-06-03 21:36:26 +0700

    Init

---
+++ Cafa/src/main/java/com/bdp/Cafa/CafaApplication.java
@@ -0,0 +1,13 @@
+package com.bdp.Cafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafaApplication.class, args);
+	}
+
+}
---
+++ Cafa/src/test/java/com/bdp/Cafa/CafaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdp.Cafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
